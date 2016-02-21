529846a79bf8784f0f0be2181d29a49d6f5fa182
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2016-02-20 19:59:36 -0800

    added support for deployment using travis

---
+++ scripts/travis-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Place ssh keys
+openssl aes-256-cbc -K $encrypted_9395ba38d102_key -iv $encrypted_9395ba38d102_iv -in deploy/webdeploy.enc -out ~/.ssh/id_rsa -d
+cp deploy/webdeploy.pub ~/.ssh/id_rsa.pub
+chmod 0400 ~/.ssh/id_rsa
+
+# Add pythonanwhere to known hosts
+ssh-keyscan -H cavejunction.dreamhost.com >> ~/.ssh/known_hosts
+
+# Deploy via rsync
+rsync -e ssh -hav --progress _site/ --compress kyleniemeyer@cavejunction.dreamhost.com:~/kyleniemeyer.com/
