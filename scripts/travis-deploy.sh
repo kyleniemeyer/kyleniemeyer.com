bfdde94d8f93088f97f4f22aad13afdd57dcaa34
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2016-02-20 20:07:35 -0800

    ...fixed additional travis commands

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Place ssh keys
-openssl aes-256-cbc -K $encrypted_9395ba38d102_key -iv $encrypted_9395ba38d102_iv -in deploy/webdeploy.enc -out ~/.ssh/id_rsa -d
-cp deploy/webdeploy.pub ~/.ssh/id_rsa.pub
+openssl aes-256-cbc -K $encrypted_9395ba38d102_key -iv $encrypted_9395ba38d102_iv -in scripts/webdeploy.enc -out ~/.ssh/id_rsa -d
+cp scripts/webdeploy.pub ~/.ssh/id_rsa.pub
 chmod 0400 ~/.ssh/id_rsa
 
 # Add pythonanwhere to known hosts
